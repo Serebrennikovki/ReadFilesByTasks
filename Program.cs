a3498a84b2e5bb60b663dae09a7e869e318c3fb3
Serebrennikovki <serebrennikov_k_i@mail.ru>
2024-07-21 23:17:24 +0500

    Добавил в репозиторий тестовые файлы и их параллельное чтение

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
-﻿namespace ReadFilesByTasks;
+﻿using System.Diagnostics;
+
+namespace ReadFilesByTasks;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var pathDirectory = "./Files";
+        var files = new DirectoryInfo(pathDirectory).GetFiles();
+        var tasks = new  List<Task<int>>();
+        var watch = new Stopwatch();
+        watch.Start();
+        foreach (var file in files)
+        {
+            Console.WriteLine(file.FullName);
+            var reader = new ReaderFile(file.FullName);
+            tasks.Add(reader.ReadFileAsync());
+        }
+
+        var result = await Task.WhenAll(tasks);
+        foreach (var spaceAmount in result)
+        {
+            Console.WriteLine(spaceAmount);
+        }
+        watch.Stop();
+        Console.WriteLine(watch.Elapsed);
     }
 }
---
+++ ReaderFile.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ReadFilesByTasks;
+
+public class ReaderFile
+{
+    private readonly string _path;
+
+    public ReaderFile(string path)
+    {
+        _path = path;
+    }
+
+    public async Task<int>ReadFileAsync()
+    {
+        using var sreamReader = new StreamReader(_path);
+        var text = await sreamReader.ReadToEndAsync();
+        var regex = new Regex(@" ");
+        MatchCollection matches = regex.Matches(text);
+        return matches.Count;
+    }
+}
